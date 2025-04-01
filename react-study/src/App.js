ba2fcc2176dfc643b9fe9eeeb9978cd8b1e38b23
진우 <128763219+Jinwoooooooo@users.noreply.github.com>
2025-04-02 04:06:21 +0900

    day1
    2번째 커밋

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  let lang = 'react';
+  let [a, b] = useState('프로그래밍 언어 추천');
+  let [logo, setLogo] = useState('ReactStudy');
 
   return (
     <div className="App">
-      <div className="container">
-        <h4 id={ lang } style={ {color : 'red', fontSize : '24px', fontWeight : 'lighter'} }>{ lang }</h4>
+      <div className="black-nav">
+        <h4>{ logo }</h4>
+      </div>
+      <div className="list">
+        <h4>{ a }</h4>
+        <p>2025년 04월 02일 발행</p>
       </div>
     </div>
   );
