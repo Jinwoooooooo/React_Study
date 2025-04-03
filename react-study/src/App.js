3af9c5dde6dae1f23e4d708b01c64cfc3a797a80
Jinwoooooooo <jinwooya55@naver.com>
2025-04-03 21:48:40 +0900

    day3
    강의는 안 봤다.
    리액트 강의 광고하는 페이지에 이런 버튼 있길래 만들어 봄. 오늘 집 가서 or 내일 일 하면서 강의 봐야지

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -8,6 +8,8 @@ function App() {
 
   let post = 'ReactStudy';
   let [글제목, b] = useState(['프로그래밍 언어 추천','Java','Python']);
+  let zero = 0;
+  let [simpleCount, simpleCountFunc] = useState(0);
   let [count, changeCount] = useState(0);
 
   return (
@@ -26,6 +28,18 @@ function App() {
       <div className="list">
         <h4>{ 글제목[2] }</h4>
         <p>2025년 04월 02일 발행</p>
+      </div>
+      <div className="list">
+        <h4>현재 카운트 :</h4>
+        <p>{ simpleCount }</p>
+        <div className="btnContainer">
+          <button onClick={() => { simpleCountFunc(simpleCount-1) }}>-1</button>
+          <button onClick={() => { simpleCountFunc(simpleCount-10) }}>-10</button>
+          <button onClick={() => { simpleCountFunc(simpleCount-100) }}>-100</button>
+          <button onClick={() => { simpleCountFunc(simpleCount+100) }}>+100</button>
+          <button onClick={() => { simpleCountFunc(simpleCount+10) }}>+10</button>
+          <button onClick={() => { simpleCountFunc(simpleCount+1) }}>+1</button>
+        </div>
       </div>
     </div>
   );
